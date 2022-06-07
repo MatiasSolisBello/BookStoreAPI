0be1283de8d656147fd43834f7dbb2763db13ac3
MatiasSolisBello <matias.solis.bello@gmail.com>
2022-06-07 09:44:44 -0400

    Startup + Peticion GET

---
+++ BookStoreAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,20 @@
+﻿using BookStoreAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Author>> Get()
+        {
+            return new List<Author>()
+            {
+                new Author() { Id = 1, Name = "Felipe" },
+                new Author() { Id = 2, Name = "Claudia" }
+            };
+        }
+    }
+}
